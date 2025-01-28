d0e302cca848ff88b80d654a7a323e6ec801721a
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2025-01-28 15:11:19 -0500

    Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from '@/components/ui/button';
+import BgGradient from '@/components/common/bg-gradient';
+import HeroSection from '@/components/home/hero-section';
 
 export default function Home() {
   return (
-    <div className="">
-      <Button variant="outline" size={'lg'}>
-        Shadcn Button
-      </Button>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+      {/* <DemoSection/> */}
+      {/* <HowItWorksSection /> */}
+      {/* <PricingSection /> */}
+      {/* <CTASection /> */}
     </div>
   );
 }
