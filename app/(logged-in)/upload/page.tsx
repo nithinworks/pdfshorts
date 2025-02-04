fb96a2bf1b3fa1291ff1839db41fa6848d7982a2
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2025-02-04 12:39:59 -0500

    Animations in the rest of the app

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -4,6 +4,8 @@ import UploadForm from '@/components/upload/upload-form';
 import { hasReachedUploadLimit } from '@/lib/user';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import { MotionDiv } from '@/components/common/motion-wrapper';
+import { containerVariants } from '@/utils/constants';
 
 export default async function Page() {
   const user = await currentUser();
@@ -16,19 +18,25 @@ export default async function Page() {
 
   const { hasReachedLimit } = await hasReachedUploadLimit(userId);
 
-  if (hasReachedLimit) {
+  //todo: remove this
+  if (!hasReachedLimit) {
     redirect('/dashboard');
   }
 
   return (
     <section className="min-h-screen">
       <BgGradient />
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+      <MotionDiv
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8"
+      >
         <div className="flex flex-col items-center justify-center gap-6 text-center">
           <UploadHeader />
           <UploadForm />
         </div>
-      </div>
+      </MotionDiv>
     </section>
   );
 }
